978d65c098f3021caeca7fd5d79aeabce50e8805
mariocos <alpacasrcool7@gmail.com>
2025-03-05 13:51:28 +0000

    for some reason it was having trouble compiling

--- pipex.h
+++ pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mariocos <mariocos@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mario <mario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:00:28 by mariocos          #+#    #+#             */
-/*   Updated: 2024/11/06 19:59:10 by mariocos         ###   ########.fr       */
+/*   Updated: 2025/03/05 13:49:04 by mario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 
 void	close_fds(int fd[2]);
 void	close_files(int files[2]);
-void	ft_pipe(int *files, char **argv, char **envp, int fd[2]);
-void	ft_child1(int *files, char **argv, char **envp, int fd[2]);
-void	ft_child2(int *files, char **argv, char **envp, int fd[2]);
+void	ft_pipe(int files[2], char **argv, char **envp, int fd[2]);
+void	ft_child1(int files[2], char **argv, char **envp, int fd[2]);
+void	ft_child2(int files[2], char **argv, char **envp, int fd[2]);
 void	err(char *str);
 int		check_infile(char **argv);
 int		check_outfile(char **argv, int argc);
